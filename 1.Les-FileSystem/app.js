657a16365dc4d4fb9107343bcd6625c013452502
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2023-04-24 23:52:11 +0100

    2.

--- 1.Les-FileSystem/app.js
+++ 1.Les-FileSystem/app.js
@@ -1,1 +1,11 @@
+const http = require("http");
 const fs = require("fs");
+ http.createServer(function(req, res){
+
+fs.readFile("1.les.html", function(err, data){
+ res.writeHead(200,{"Content-Type": "text/html"});
+res.write(data);
+res.end();
+
+})
+ }).listen(8080);
