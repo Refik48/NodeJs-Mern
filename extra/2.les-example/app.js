32071e86873499904572513cfb4ab9f648275b47
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2023-06-21 23:25:45 +0100

    21/06

---
+++ extra/2.les-example/app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const hurl = require("url");
+const fs = require("fs");
+
+http.createServer(function (req, res) {
+
+
+}).listen(8080);
+console.log("merhaba Node.js");
